1ec2757ea19edee9350c4ba1467ec85d467c1f64
Parsa Soleimani <61251770+parsoli83@users.noreply.github.com>
2020-09-07 14:30:05 +0430

    Add files via upload

---
+++ vidoal_links.py
@@ -0,0 +1,7 @@
+vidoal_links=[
+    "https://aspb13.cdn.asset.aparat.com/aparat-video/32fa0b35a0daf630bfa0c9a1f8ef2f429583346-720p.mp4",
+    "https://aspb11.cdn.asset.aparat.com/aparat-video/bed4aab1db96fd8a9edf91c3f138e9e311960111-720p.mp4",
+    "https://aspb13.cdn.asset.aparat.com/aparat-video/1dc2b44c4fc35decaf0dbe5bc1dae3bd7159018-720p.mp4",
+    "https://as3.cdn.asset.aparat.com/aparat-video/9675678bc72ebf0dc7afb7dfa2920e676838250-720p.mp4",
+    "https://as5.cdn.asset.aparat.com/aparat-video/82009332ee3584b88e77a1ab38fbe2789781375-720p.mp4"
+]
